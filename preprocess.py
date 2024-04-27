cb00db720ce44b476ec5a8905d31567a33b9bd68
vicksvinegar <victoria_j_chen@brown.edu>
2024-04-26 21:09:23 -0400

    preprocessing

--- preprocess.py
+++ preprocess.py
@@ -4,8 +4,7 @@ import librosa.display
 import matplotlib.pyplot as plt
 import numpy as np
 import pretty_midi
-
-SAMPLING_RATE = 22050
+from globals import SAMPLING_RATE, SECONDS, BINS_PER_OCTAVE, START_TOKENS, END_TOKENS
 
 def lame_process(input_file, output_file):
     '''
@@ -30,9 +29,7 @@ def convert_mp3_to_wav(input_path, output_path):
     else:
         lame_process(input_path, output_path)
 
-#base minimum frequency off of midi min freq? 
-#need to iron out the inputs for the cqt 
-def wav_to_spectrogram(wav_file, sr = SAMPLING_RATE, spec_type = "cqt", hop_length = 512):
+def wav_to_spectrogram(wav_file, sr, seconds, bins_per_octave):
     """
     Converts a wav file into a tensor input for transformer model
     param wav: path to a wav file 
@@ -40,18 +37,14 @@ def wav_to_spectrogram(wav_file, sr = SAMPLING_RATE, spec_type = "cqt", hop_leng
     y, s = librosa.load(wav_file)
     print(f'sampling rate: {sr}')
 
-    if spec_type == "cqt":
-        spectrogram = librosa.cqt(y, sr = sr, bins_per_octave = 36) #only have to define either n_bins or bins_per_octave
-    else:
-        spectrogram = librosa.feature.melspectrogram(y = y, sr = sr, n_fft = 2048, hop_length = 512)
-
+    spectrogram = librosa.cqt(y, sr = sr, bins_per_octave = bins_per_octave) #only have to define either n_bins or bins_per_octave
     spectrogram = librosa.amplitude_to_db(np.abs(spectrogram), ref = np.max) #convert to dB scale 
 
     #think about the tokens for <start> <token> 
     
-    print(f'spec shape: {spectrogram.shape}') #(num_freq_bins, num_time_frames)
+    #print(f'spec shape: {spectrogram.shape}') #(num_freq_bins, num_time_frames)
     spectrogram = spectrogram.T #transpose spectrogram to get right order for transformer (num_time_frames, num_freq_bins)
-    print(f'spec shape: {spectrogram.shape}')
+    #print(f'spec shape: {spectrogram.shape}')
     #plot_spectrogram(spectrogram, sr)
     
     minDB = np.min(spectrogram)
@@ -59,31 +52,28 @@ def wav_to_spectrogram(wav_file, sr = SAMPLING_RATE, spec_type = "cqt", hop_leng
     #print(f'Minimum: {np.min(spectrogram)}, Maximum: {np.max(spectrogram)}, Mean: {np.mean(spectrogram)}') 
     #spectrogram = np.pad(spectrogram, ((window_size//2,window_size//2),(0,0)), 'constant', constant_values=minDB)  #pad audio
     
-    window_size = librosa.time_to_frames(5, sr = sr)
+    window_size = librosa.time_to_frames(seconds, sr = sr)
     windows = []
 
     pad_width = ((0, window_size - (spectrogram.shape[0] % window_size)), (0,0))
     spectrogram = np.pad(spectrogram, pad_width, 'constant', constant_values=minDB)
-    print(f'Padded spectrogram shape: {spectrogram.shape}')
-
-    i = 0
-    start_frames = []
-
+    #print(f'Padded spectrogram shape: {spectrogram.shape}')
+    
     for i in range(0, spectrogram.shape[0], window_size):
         w = spectrogram[i:i + window_size, :]
-        start_frames.append(i)
         windows.append(w)
     
     windows = np.array(windows) 
-    start_frames = np.array(start_frames)
-    print(f'windowed shape: {windows.shape}')
+
+    #Add start and end tokens to the beginning and end of each sequence
+    pad_start_tokens = ((0,0), (1, 0), (0, 0))
+    windows = np.pad(windows, pad_start_tokens, 'constant', constant_values= START_TOKENS)
+
+    pad_end_tokens = ((0,0), (0, 1), (0, 0))
+    windows = np.pad(windows, pad_end_tokens, 'constant', constant_values= END_TOKENS)
 
     #plot_spectrogram(spectrogram_db, sr) '''
-    return windows, start_frames
-
-def get_times(frames, sr):
-    times = librosa.frames_to_time(frames, sr=sr)
-    return times 
+    return windows
 
 def plot_spectrogram(spectrogram, sr):
     """
@@ -96,41 +86,35 @@ def plot_spectrogram(spectrogram, sr):
     plt.tight_layout()
     plt.show()
 
-def midi_to_np(midi_file):
-    pm_mid = pretty_midi.PrettyMIDI(midi_file)
-    piano_roll = pm_mid.get_piano_roll(fs=SAMPLING_RATE).T
-    piano_roll[piano_roll > 0] = 1
-    print(f'pianoroll non-padded shape: {piano_roll.shape[0]}')
+def midi_to_piano_roll(midi_file_path, sr, start_pitch=19, end_pitch=107):
+    '''
+    Returns an np array of the piano roll representation of a midi file, 
+    with 88 notes representing those of a piano keyboard rather than 
+    the default 128 notes, that is, from MIDI note 21 (A0) to MIDI note 108 (C8).
+    '''
+    midi_data = pretty_midi.PrettyMIDI(midi_file_path)
 
-    remainder = 5*SAMPLING_RATE - (piano_roll.shape[0] % (5*SAMPLING_RATE))
+    raw_piano_roll = midi_data.get_piano_roll(fs=sr)[start_pitch:end_pitch]
+    # 0 meaning not played -> converting it into binary representation
+    piano_roll = raw_piano_roll > 0
+    piano_roll = np.asarray(piano_roll).astype(int).T
+    #print(f'shape of unpadded pianoroll: {piano_roll.shape}')
+    #print(f'size of unpadded pianoroll in GB: {piano_roll.itemsize * piano_roll.size}')
+
+
+    remainder = SECONDS*SAMPLING_RATE - (piano_roll.shape[0] % (SECONDS*SAMPLING_RATE))
     pad_width = ((0, remainder), (0,0))
-    print(pad_width)
     piano_roll = np.pad(piano_roll, pad_width, 'constant', constant_values=0)
-    print(f'pianoroll padded shape: {piano_roll.shape}')
-    piano_roll = np.reshape(piano_roll, (-1, 5 * SAMPLING_RATE, piano_roll.shape[1]))
+    piano_roll = np.reshape(piano_roll, (-1, SECONDS * SAMPLING_RATE, piano_roll.shape[1]))
+    #print(f'shape of pianoroll after splitting: {piano_roll.shape}')
+    #print(f'size of padded/split pianoroll in GB: {piano_roll.itemsize * piano_roll.size }')
     return piano_roll
 
     
 def main():
     #convert_mp3_to_wav("data/mp3", "data/wav") 
-    cqt1, start_frames = wav_to_spectrogram("data/wav/bach1.wav", spec_type= "mel")
-    print(get_times(start_frames, SAMPLING_RATE)[:5])
-    start_times = librosa.frames_to_time(start_frames, sr=SAMPLING_RATE)
-    
-    #print(f'times max: {np.max(times)}, times:min {np.min(times)}')
-    #print(f'times shape: {times.shape}')
-
-    pm = pretty_midi.PrettyMIDI("data/midi/bach1.mid")
-    #times = range(start_times[0], start_times[1], 1/SAMPLING_RATE)
-    #print(len(times))
-
-    pianoroll = pm.get_piano_roll(fs=SAMPLING_RATE,times=None) 
-    pianoroll_time = midi_to_np("data/midi/bach1.mid")
-
-    print(f'pianoroll w/o time: {pianoroll.shape}')
-    print(f'pianoroll w/ time: {pianoroll_time.shape}')
-
-    #spectrogram2 = wav_to_spectrogram("data/wav/bach2.wav", spec_type= "mel")
+    cqt1 = wav_to_spectrogram("data/wav/bach1.wav", SAMPLING_RATE, SECONDS, BINS_PER_OCTAVE)
+    pm1 = midi_to_piano_roll("data/midi/bach1.mid", SAMPLING_RATE)
 
 if __name__ == '__main__':
     main()
