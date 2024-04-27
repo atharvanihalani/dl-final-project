ab1119efc0dd13003b462eca63ae98fd42efcfc9
SiddhartaL <siddharta_laloux@brown.edu>
2024-04-27 18:49:40 -0400

    finished implementing transformer decoder

--- model/miscellaneous_layers.py
+++ model/miscellaneous_layers.py
@@ -28,6 +28,7 @@ class FeedForward(tf.keras.layers.Layer):
         return out
     
 def positional_encoding(length, depth):
+    """Copied from Siddharta Laloux's HW5p"""
     ## Can remove signature
     depth = depth/2
     ## Generate a range of positions and depths 
@@ -42,6 +43,7 @@ def positional_encoding(length, depth):
 
 
 class PositionalEncoding(tf.keras.layers.Layer):
+    """Copied from Siddharta Laloux's HW5p"""
     def __init__(self, vocab_size, embed_size, window_size):
         super().__init__()
         self.embed_size = embed_size
